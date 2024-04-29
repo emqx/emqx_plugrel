7162c262e2cbdd5dcd4faab4ffc014a972f18b05
JimMoen <LnJimMoen@outlook.com>
2024-04-29 23:17:36 +0800

    fix: prevents forward slashes in a JSON string from being escaped

--- src/emqx_plugrel.erl
+++ src/emqx_plugrel.erl
@@ -125,7 +125,7 @@ make_tar(#{name := Name, rel_vsn := Vsn, rel_apps := Apps} = Info, State) ->
     ok = filelib:ensure_dir(InfoFile),
     ok = maybe_copy_files(LibDir),
     %% write info file
-    ok = file:write_file(InfoFile, jsone:encode(Info, [{space, 1}, {indent, 4}])),
+    ok = file:write_file(InfoFile, jsone:encode(Info, [native_forward_slash, {space, 1}, {indent, 4}])),
     %% copy apps to lib dir
     Sources = lists:map(fun(App) -> filename:join([BaseDir, "rel", Name, "lib", App]) end, Apps),
     ok = rebar_file_utils:cp_r(Sources, LibDir),
